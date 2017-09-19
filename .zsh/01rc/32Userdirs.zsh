fccba0044defd81ba747d10fe359852ddbe233c4
Jean Chalard <jchalard@jchalard.tok.corp.google.com>
2017-09-19 15:07:08 +0900

    Fix for when there are too many users.
    This will take as much time as it takes to evaluate the var,
    which is ~1 sec on my google computer because there are thousands
    of users. Also fix an egregious typo.

--- .zsh/01rc/32Userdirs.zsh
+++ .zsh/01rc/32Userdirs.zsh
@@ -1,3 +1,4 @@
-# Enumarate user dirs so that they are replaced by the prompt even if
+#CONF static [[ $HOST != *.corp.google.com ]]
+# Enumerate user dirs so that they are replaced by the prompt even if
 # they are not directly named
 : $userdirs
