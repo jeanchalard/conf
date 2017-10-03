84bdb51c1338f44dcc030d9d1d0de8f139641560
Chalard Jean <jean.chalard@gmail.com>
2017-10-03 14:57:20 +0900

    Add the kdialog helpers, to warn myself gently.

---
+++ .zsh/05alias/Kdialog.zsh
@@ -0,0 +1,23 @@
+#CONF static which kdialog
+popup() {
+  timeout=${3-5}
+  if [[ $2 ]]
+  then
+    title=$1
+    msg=$2
+  else
+    title='Notice'
+    msg=$1
+  fi
+  kdialog --title $title --passivepopup $msg $timeout
+}
+statuspopup() {
+   if [[ 0 = $? ]]
+  then
+    title=${1-Status}
+    popup $title Success
+  else
+    title=${1-Status}
+    popup $title Failure
+  fi
+}
